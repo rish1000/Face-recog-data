2e3e2e047941bbbdb3439c31edd2170ae578034b
rish1000 <95483197+rish1000@users.noreply.github.com>
2021-12-03 19:14:43 +0530

    Add files via upload

---
+++ face data collector.py
@@ -0,0 +1,54 @@
+import numpy as np
+import cv2
+
+
+cap = cv2.VideoCapture(0) # object to interact with web cam
+
+face_cascade = cv2.CascadeClassifier("D:\All downloads\Edge Downloads\haarcascade_frontalface_alt.xml")
+
+X = []
+
+padding = 10
+while True:
+
+    ret, image = cap.read()
+
+    if ret == False:
+        continue
+
+    # detect faces in the image
+    faces = face_cascade.detectMultiScale(image)
+
+    print(faces)
+    for face in faces:
+        x, y, w, h = face
+        #cv2.rectangle(image, (x-padding, y-padding), (x+w+padding, y+h+padding), (0,0,255),2) # draw rectangle on an image(images, start pt)
+
+        #slice the face out of the image
+        face_section = image[y-padding:y+h+padding, x-padding:x+w+padding]
+        face_section = cv2.resize(face_section, (100,100))
+
+    # cv2.imshow("Captured image", image)
+    cv2.imshow("face_section", face_section)
+    # print(face_section.shape)
+
+    # append the face section to my x-list
+    X.append(face_section.reshape(1,-1)) # rather use flatten() or reshape(-1,)
+
+    print(len(X), X[-1].shape)
+
+    # exit out of the loop
+    key_pressed = cv2.waitKey(25)
+    # print(key_pressed, type(key_pressed))
+    if key_pressed == ord('q'):
+        print(key_pressed)
+        print("Q was pressed")
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+# convert the X list to numpy array
+X = np.array(X)
+filename = input("enter the name of the person: ")
+np.save(filename, X)
